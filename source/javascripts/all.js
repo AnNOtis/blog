10c59f45af0d5a01a65d43babb3c8ce90a1acda7
Otis <otis@hdtech.com.tw>
2014-07-31 00:11:43 +0800

    fix footer

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,21 @@
 //= require_tree .
+
+$().ready(function() {
+  resetBodyHeight();
+  $(window).resize(function() {
+    resetBodyHeight();
+  });
+  function resetBodyHeight(){
+    var bodyHeight = $('body').height();
+    var windowHeight = $(window).height();
+    if(bodyHeight<windowHeight){
+      $('.footer').addClass('fixed-bottom');
+    }else{
+      $('.footer').removeClass('fixed-bottom');
+    }
+  }
+
+
+
+
+});
