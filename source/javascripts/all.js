17228d91c7a742c390aa92fe6687cc033f584e23
Otis <otis@hdtech.com.tw>
2015-03-08 18:34:16 +0800

    Exclude javascript file, because useless.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,25 +1,25 @@
 //= require_tree .
 
-$().ready(function() {
-  resetBodyHeight();
-  $(window).resize(function() {
-    resetBodyHeight();
-  });
-  function resetBodyHeight(){
-    var bodyHeight = $('body').height();
-    console.log(bodyHeight)
-    var windowHeight = $(window).height();
-    console.log(windowHeight)
-    if(bodyHeight<windowHeight){
-      $('footer').addClass('fixed-bottom');
-    }else{
-      $('footer').removeClass('fixed-bottom');
-    }
-  }
+// $().ready(function() {
+//   resetBodyHeight();
+//   $(window).resize(function() {
+//     resetBodyHeight();
+//   });
+//   function resetBodyHeight(){
+//     var bodyHeight = $('body').height();
+//     console.log(bodyHeight)
+//     var windowHeight = $(window).height();
+//     console.log(windowHeight)
+//     if(bodyHeight<windowHeight){
+//       $('footer').addClass('fixed-bottom');
+//     }else{
+//       $('footer').removeClass('fixed-bottom');
+//     }
+//   }
 
 
-  $('#menu').click(function() {
-    $('nav').toggleClass('is_active');
-  });
+//   $('#menu').click(function() {
+//     $('nav').toggleClass('is_active');
+//   });
 
-});
+// });
